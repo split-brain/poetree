0f542c1846d82e12956e9f2e56461d518b1cfb79
Oleh Palyanytsya <oleh.palianytsia@gmail.com>
2015-12-05 14:30:01 +0200

    Add dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,18 @@
   :url "http://clojurecup.com/poetree"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
+
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring "1.4.0"]
+                 [http-kit "2.1.18"]
+                 [hiccup "1.0.5"]
+
+                 [com.taoensso/timbre "4.1.4"]
+
+                 [org.clojure/core.match "0.3.0-alpha4"]
+
+                 [clj-oauth "1.5.2"]
+                 [twitter-api "0.7.8"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler poetree.handler/app}
   :profiles
