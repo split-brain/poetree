4986975c7a884e1e4a4be4b80d1fb17c7d34ebe7
Dmitriy Morozov <dmmo@ciklum.com>
2015-12-05 14:25:08 +0200

    Added column  for poems table. Added few more haiku examples.

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -16,16 +16,31 @@ create table poems (
   line_order integer not null,
   type       varchar(100) not null,
   content    text not null,
+  lang       text not null,
   users_id   integer references users(id),
   poems_id   integer references poems(id)
 );
 
-insert into poems (line_order, type, content, users_id, poems_id) 
-            values (1, 'HAIKU', 'Little semicolon', 1, null);
-insert into poems (line_order, type, content, users_id, poems_id) 
-            values (2, 'HAIKU', 'that makes me not compile this,', 1, 1);
-insert into poems (line_order, type, content, users_id, poems_id) 
-            values (3, 'HAIKU', 'where are you missing?', 1, 2);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (1, 'HAIKU', 'Little semicolon', 'EN', 1, null);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (2, 'HAIKU', 'that makes me not compile this', 'EN', 1, 1);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (3, 'HAIKU', 'where are you missing?','EN', 1, 2);
+
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (1, 'HAIKU', 'Start in the middle', 'EN', 1, null);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (2, 'HAIKU', 'Then go halfway up or down', 'EN', 1, 1);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (3, 'HAIKU', 'Repeat until done','EN', 1, 2);
+
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (1, 'HAIKU', 'World is vast and wide', 'EN', 1, null);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (2, 'HAIKU', 'So much out there to explore', 'EN', 1, 1);
+insert into poems (line_order, type, content, lang, users_id, poems_id)
+values (3, 'HAIKU', 'Right now, let''s eat lunch','EN', 1, 2);
 
 create table likers (
   poems_id   integer references poems(id),
