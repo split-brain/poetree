6eacfb19cbf54535fec51793c21124b13ded4d86
Oleh Palyanytsya <oleh.palianytsia@gmail.com>
2015-12-05 14:42:58 +0200

    Add server startup

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
 
                  [compojure "1.4.0"]
                  [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
                  [http-kit "2.1.18"]
                  [hiccup "1.0.5"]
 
--- src/poetree/handler.clj
+++ src/poetree/handler.clj
@@ -1,7 +1,9 @@
 (ns poetree.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+
+            [org.httpkit.server :as http-server]))
 
 (defroutes app-routes
   (GET "/" [] "Welcome to Poetree")
@@ -9,3 +11,10 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn start-server [app]
+  (let [port (Integer/parseInt
+              (or (System/getenv "PORT") "8080"))]
+    (http-server/run-server app {:port port :join? false})))
+
+(def server (start-server app))
