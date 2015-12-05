8f77dc43d18fb2dbbf500156e8171cb3a57c3f71
Dmitriy Morozov <dmmo@ciklum.com>
2015-12-05 18:15:03 +0200

    Logging

--- src/poetree/config.clj
+++ src/poetree/config.clj
@@ -15,7 +15,7 @@
 (defn load-config
   "Given a filename, load a config file and set app config."
   [path]
-  (println "Loading config from " path)
+  (println "Reading config from" path)
   (let [config (-> path
                    slurp
                    edn/read-string
